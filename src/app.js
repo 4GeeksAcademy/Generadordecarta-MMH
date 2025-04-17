4dfa55902b2d1b54ba97b53918b5848d1d65f9a7
Dolphinh16 <miguelhmercedes@hotmail.com>
2025-04-17 21:53:57 +0000

    Cardsgenerator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  muestraCartas()
+document.getElementById("generar").addEventListener("click",muestraCartas);
+
+
+};
+
+
+
+
+
+function muestraCartas() {
+
+  let cartasIconos = ['♦', '♠', '♥', '♣'];
+  let numeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  let iconoAleatorio = cartasIconos[Math.floor(Math.random() * cartasIconos.length)];
+  let numeroAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
+
+
+  const elementoIconoArriba = document.getElementById("icono-arriba");
+  const elementoNumero = document.getElementById("numero");
+  const elementoIconoAbajo = document.getElementById("icono-abajo");
+
+
+  elementoIconoArriba.innerHTML = iconoAleatorio
+  elementoNumero.innerHTML = numeroAleatorio
+  elementoIconoAbajo.innerHTML = iconoAleatorio
+
+
+  if (iconoAleatorio == '♦' || iconoAleatorio == '♥') {
+    elementoIconoAbajo.style.color = "red";
+    elementoIconoArriba.style.color = "red";
+
+  } else {
+    elementoIconoAbajo.style.color = "black";
+    elementoIconoArriba.style.color = "black";
+
+  }
+
+
 };
